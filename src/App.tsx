28f7bc6e29ea1845a6add759bbe0ea1618394edd
rashmipanday06 <rashmipanday06598@gmail.com>
2025-06-21 17:16:54 +0530

    feat: implement React Router for page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,28 @@ import { useState } from 'react'
 import Registation from './components/pages/registation'
 import Login from './components/pages/login'
 import Home from './components/pages/home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>{/* <Registation/> */}
-{/* <Login/> */}
-<Home/>
-</>
+
+    <BrowserRouter>
+    <Navbar/>
+      <div>
+        <Routes>
+          <Route  path="/" element={<Home />}/>
+          <Route  path="/login" element={<Login />}/>
+          <Route  path="/Registation" element={<Registation />}/>
+        </Routes>
+      </div>
+   
+    
+    </BrowserRouter>
+
+
 
   )
 }
--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,15 +1,17 @@
 import focusBoard from '../../../assets/programming.png'
-import { Link } from 'react-router-dom';
 import style from './style.module.css'
 
 const Home = () => {
   return (
     <div> 
    <div className={style.container}>  
-    <div>
-     <h2>Organize your tasks. Focus on what matters.</h2>
-     <p>FocusBoard helps you track tasks, set goals, and get more done.</p>
-     <button>Get Started</button>
+    <div className={style.containerWrapper}>
+    <h1 className={style.heroHeading}>
+  Organize your tasks. <br />
+  Focus on what matters.
+</h1>
+     <p className={style.subTitle}>FocusBoard helps you track tasks, set goals, and get more done.</p>
+     <button className={style.btn}>Get Started</button>
      </div>
         <img src={focusBoard} alt="Focus Board Preview" className={style.iconWrapper} />
     </div>
